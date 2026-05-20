bee753c6dc600eeb4bac0df155219d13ee4fb70d
Aditya Gupta <manuagarwal1000@gmail.com>
2026-05-20 22:35:47 +0530

    feat : add the dark theme support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
 export default function Home() {
-  return <div></div>;
+  return <div className="grid grid-cols-3  ">
+    <div>
+     left sidebar
+    </div>
+    <div className="col-span-2">
+      Main feed
+    </div>
+    <div>
+     right content
+    </div>
+  </div>;
 }
